40d59bcb10a0e541b6bec670776a031d2e9155f6
SANADS1 <yarazaq99@gmail.com>
2020-04-14 17:15:11 +0100

    impact code for challenges1-3

--- src/estimator.js
+++ src/estimator.js
@@ -11,6 +11,12 @@ const covid19ImpactEstimator = (data) => {
     } else if (periodType === 'months') {
       timeToElapse *= 30;
     }
+    const infectionsByRequestedTime = currentlyInfected  (2 * (Math.trunc(timeToElapse / 3)));
+    const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.15);
+    const hospitalBedsByRequestedTime = Math.trunc((totalHospitalBeds * 0.35) - severeCasesByRequestedTime);
+    const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
+    const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
+    const dollarsInFlight = Math.trunc((infectionsByRequestedTime,  avgDailyIncomePopulation,  avgDailyIncomeInUSD) / timeToElapse);
 };
 
 export default covid19ImpactEstimator;
