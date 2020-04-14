d90eeea093e76aa5aec7bf005d7421d1b50c983f
SANADS1 <yarazaq99@gmail.com>
2020-04-14 18:49:25 +0100

    currently infected adjusted

--- src/estimator.js
+++ src/estimator.js
@@ -30,7 +30,7 @@ const covid19ImpactEstimator = (data) => {
   } else if (periodType === 'months') {
     timeToElapse *= 30;
   }
-  const infectionsByRequestedTime = currentlyInfected(2 * (Math.trunc(timeToElapse / 3)));
+  const infectionsByRequestedTime = currentlyInfected * (2 ** (Math.trunc(timeToElapse / 3)));
   const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.15);
   const hospitalBedsByRequestedTime = Math.trunc((totalHospitalBeds * 0.35) - severeCasesByRequestedTime);
   const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
