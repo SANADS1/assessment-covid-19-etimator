4d390a79fa5aafa6f0b6b3e5706a5631e1701209
SANADS1 <yarazaq99@gmail.com>
2020-04-14 17:13:28 +0100

    first variable declaration

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,16 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const { reportedCases, periodType, totalHospitalBeds } = data;
+    let { timeToElapse } = data;
+    const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = data.region;
+  
+    const currentlyInfected = reportedCases * 10;
+    const svCurrentlyInfected = reportedCases * 50;
+  
+    if (periodType === 'weeks') {
+      timeToElapse *= 7;
+    } else if (periodType === 'months') {
+      timeToElapse *= 30;
+    }
+};
 
 export default covid19ImpactEstimator;
