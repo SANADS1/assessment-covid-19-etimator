9bb9a5c6d4bb050a9a0f2b14d0e79ed46ea57126
SANADS1 <yarazaq99@gmail.com>
2020-04-14 17:20:01 +0100

    modified impact return data

--- src/estimator.js
+++ src/estimator.js
@@ -35,7 +35,7 @@ const covid19ImpactEstimator = (data) => {
             casesForICUByRequestedTime,
             casesForVentilatorsByRequestedTime,
             dollarsInFlight
-          },
+        },
     }
   
 };
